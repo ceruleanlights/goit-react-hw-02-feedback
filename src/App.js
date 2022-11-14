d813717080770b573ea8ab780cde3369fdd3db30
ceruleanlights <arestus2@gmail.com>
2022-11-14 15:42:50 +0100

    change initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
+import Statistics from "./components/Statistics/Statistics";
+import Section from "./components/Section/Section";
+import Notification from "./components/Notification/Notification";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const positivePercentage = (this.state.good / this.countTotalFeedback()) * 100;
+    return positivePercentage;
+  }
+
+  onLeaveFeedback = (e) => {
+    const stateButtonName = e.target.textContent.toLowerCase();
+    this.setState((prevState) => {
+      for (const key in prevState) {
+        if (key === stateButtonName) {
+          return { [key]: prevState[key] + 1 };
+        }
+      }
+    });
+  };
+
+  render() {
+    const totalFeedback = this.countTotalFeedback();
+    const positiveFeedback = this.countPositiveFeedbackPercentage(totalFeedback).toFixed();
+    const { good, neutral, bad } = this.state;
+    return (
+      <div className="App">
+        <Section title="Please leave feedback">
+          <FeedbackOptions options={["good", "neutral", "bad"]} onLeaveFeedback={this.onLeaveFeedback} />
+        </Section>
+        <Section title="Statistics">
+          {totalFeedback > 0 ? (
+            <Statistics
+              good={good}
+              bad={bad}
+              neutral={neutral}
+              countTotalFeedback={totalFeedback}
+              countPositiveFeedbackPercentage={positiveFeedback}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </div>
+    );
+  }
 }
 
 export default App;
